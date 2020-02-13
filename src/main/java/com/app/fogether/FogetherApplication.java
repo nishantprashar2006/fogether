4240ab89e510f55f5d1ce086d29f0d7e70f858ef
Dikshit vig <dikshit.vig@gmail.com>
2020-02-13 01:15:23 -0500

    first commit

---
+++ src/main/java/com/app/fogether/FogetherApplication.java
@@ -0,0 +1,13 @@
+package com.app.fogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FogetherApplication.class, args);
+	}
+
+}
